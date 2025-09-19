9dc60245e045e4d55a84080bcb3ed2692df0bf32
YogeshBogati <109842315+YogeshBogati@users.noreply.github.com>
2025-09-19 15:18:32 -0500

    Add Chapter 3 with story code

---
+++ chapter3.py
@@ -0,0 +1,10 @@
+# chapter_3.py
+
+# The glowing sword began to blink with commit messages.
+
+def sword_commits():
+    commits = ["Fixed a bug", "Refactored destiny", "Added unit tests for fate"]
+    for commit in commits:
+        print(f"The sword glowed: {commit}")
+
+sword_commits()
